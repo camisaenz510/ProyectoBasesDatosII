8634c8f1a581fa68568db5754423c5fa218c3bde
marivar08 <mariana0810@hotmail.es>
2022-11-12 11:50:18 -0600

    Gerente general consultar
    base del javascript con el modal para el consultar del gerente general

---
+++ Gerente General/Consultar producto/script.js
@@ -0,0 +1,27 @@
+var url = "/GerenteGeneral/"//Objeto para leer posibles respuestas del servidor.
+
+var EstadosDeRespuesta = {
+    StatusCode: 0,
+    StatusDescription: ''
+};
+
+
+function consultarProducto() {
+    $('#detalleProducto').modal('show');
+   /* fetch(url + "GerenteGeneral_Consultar") //Nombre pendiente a ver cual le pone Santiago
+        .then((respuesta) => {
+            return respuesta.json();
+        }).then((datos) => {
+            var producto = '';
+            //En la variable producto se pone la información en lenguaje html que se mostrará en el modal de Bootstrap
+            document.getElementById("listaMarcas").innerHTML = ptoducto
+        });*/
+}
+
+
+
+
+
+
+
+
