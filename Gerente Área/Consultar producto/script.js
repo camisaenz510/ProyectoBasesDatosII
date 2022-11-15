dae5508f43dba9173af07f756e752c3314f9527a
marivar08 <mariana0810@hotmail.es>
2022-11-14 22:54:12 -0600

    base js para gerente área

---
+++ Gerente Área/Consultar producto/script.js
@@ -0,0 +1,21 @@
+var url = "/GerenteDeArea/"//Objeto para leer posibles respuestas del servidor.
+
+var EstadosDeRespuesta = {
+    StatusCode: 0,
+    StatusDescription: ''
+};
+
+
+function consultarProducto() {
+    $('#detalleProducto').modal('show');
+   /* fetch(url + "GerenteDeArea_Consultar") //Nombre pendiente a ver cual le pone Santiago
+        .then((respuesta) => {
+            return respuesta.json();
+        }).then((datos) => {
+            var producto = '';
+            //En la variable producto se pone la información en lenguaje html que se mostrará en el modal de Bootstrap
+            document.getElementById("listaMarcas").innerHTML = producto
+        });*/
+}
+
+
